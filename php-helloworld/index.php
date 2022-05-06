0d4eadbd88e285eb82a9bfa3493425f2e4c90d9b
Student User <student@workstation.lab.example.com>
2022-05-06 00:49:24 -0400

    updated app

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,5 @@
 <?php
-print "Hello, World! This is johnny's first openshift container app " . "\n";
+print "Hello, World! This is johnny's first openshift container app " . "\r\n";
 print "Ikeda is an a-hole" . "<br>";
 print "And has no idea what he's doing. EVER!" . "\n";
 echo "Hello world this is example \r\n in php.";
