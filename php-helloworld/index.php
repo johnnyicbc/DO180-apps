cf0d20342fedb79df047c44b38c20dd01ca890db
Student User <student@workstation.lab.example.com>
2022-05-06 01:06:49 -0400

    updated app

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,7 @@
 <?php
+print "Hello, World! This is johnny's first openshift container app " . "<br>";
+print "Ikeda is an a-hole" . "<br>";
+print "And has no idea what he's doing. EVER!" . "<br>";
 echo "Hello world this is example \r\n in php.";
-echo "<br>";
-echo nl2br("You will find the \n newlines in this string \r\n on the browser window.");
+echo "Hello world this is example \r\n in php.";
 ?>
